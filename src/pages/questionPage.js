83471a99b6a17bbc668126559f933d799c02e84a
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-01 19:13:09 +0100

    check with prettier and update import

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,7 +1,11 @@
 import { quizData } from '../data.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
-import { USER_INTERFACE_ID } from '../constants.js';
+import {
+  USER_INTERFACE_ID,
+  ANSWERS_LIST_ID,
+  NEXT_QUESTION_BUTTON_ID,
+} from '../constants.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
