178ed06485077b332b49d97283216ca913e85cbf
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-02 13:18:14 +0100

    display answers

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -7,12 +7,14 @@ export const createAnswerElement = (key, answerText) => {
   const listItem = document.createElement('li');
   const answerInput = document.createElement('input');
   const label = document.createElement('label');
+  const answer = document.createTextNode(answerText);
   listItem.appendChild(answerInput);
   answerInput.setAttribute('type', 'radio');
   answerInput.setAttribute('id', key);
   answerInput.setAttribute('name', 'answer');
   answerInput.setAttribute('value', answerText);
   answerInput.appendChild(label);
+  listItem.appendChild(answer);
   label.setAttribute('for', key);
   label.innerText = answerText;
 
