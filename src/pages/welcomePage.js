3a51184645c1946da9ad17bb14e650f40e85612d
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-04 18:17:22 +0100

    if user does  not enter a name console will alert and start quiz qill not work

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -20,6 +20,9 @@ export const initWelcomePage = () => {
 };
 
 const startQuiz = () => {
-  quizData.userName = document.getElementById(INPUT_NAME).value;
-  initQuestionPage();
+  if ((quizData.userName = document.getElementById(INPUT_NAME).value === '')) {
+    alert('Please enter your name!');
+  } else {
+    initQuestionPage();
+  }
 };
