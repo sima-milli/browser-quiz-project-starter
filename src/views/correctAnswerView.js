88b4ce80362335f4e7adb05f5aa821a5a0a55cc6
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-04 15:24:44 +0100

    add correct score, disables button if answer is not changed, set currentindex and score to 0 when redo the test

--- src/views/correctAnswerView.js
+++ src/views/correctAnswerView.js
@@ -1,11 +1,10 @@
-// correct answer view
+import { quizData } from '../data.js';
 
 // this function will get the answer provided by user
-const getSelected = () => {
+export const getSelected = () => {
   // we can use this function in the questions view to show the rocket when user select the correct answer after each question
+  let answer;
   let answerEls = document.querySelectorAll('input[name="answer"]');
-  let answer = 0;
-  console.log('answerEls', answerEls);
   answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
       // we have to use checked here because we are getting all 4 inputs
@@ -15,19 +14,12 @@ const getSelected = () => {
   return answer;
 };
 
-// this function is to check if the selected answer is the correct one
-const getScore = () => {
-  let score = 0;
-  const answer = getSelected();
-  if (answer.length) {
-    for (let i = 0; i < answer.length; i++) {
-      if (answer[i] === quizData.questions[i].correct) {
-        score++;
-      }
-    }
+export const getScore = () => {
+  let answer = getSelected();
+  if (answer === quizData.questions[quizData.currentQuestionIndex].correct) {
+    quizData.scoreCount++;
   }
   // you can use else to do the steps for wrong answer (rocket going down for example)
-  return score;
 };
 
 // if you need more steps for correct answer flow it should go here
--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -1,41 +1,7 @@
 import { BACK_TO_WELCOME_PAGE_BUTTON_ID } from '../constants.js';
 import { quizData } from '../data.js';
 
-// ===> we can delete `getSelected` and `getScore` from here because they are now moved to correct view
-
-const getSelected = () => {
-  // we can use this function in the questions view to show the rocket when user select the correct answer after each question
-  let answerEls = document.querySelectorAll('input[name="answer"]');
-  let answer = 0;
-  console.log('answerEls', answerEls);
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      // we have to use checked here because we are getting all 4 inputs
-      answer = answerEl.id;
-    }
-  });
-  console.log('answer1: ', answer);
-  return answer;
-};
-
-const getScore = () => {
-  let score = 0;
-  const answer = getSelected();
-  console.log('answer2: ', answer);
-  if (answer.length) {
-    for (let i = 0; i < answer.length; i++) {
-      if (answer[i] === quizData.questions[i].correct) {
-        score++;
-      }
-    }
-  }
-
-  console.log('score: ', score);
-  return score;
-};
-
-export const createResultsElement = () => {
-  const score = getScore();
+export const createResultsElement = (score) => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
     <h2>${quizData.userName} You answered ${score} correct answers out of ${quizData.questions.length} questions</h2>
