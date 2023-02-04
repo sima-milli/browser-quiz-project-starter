d48cea9300b55f657b4650bab18298f329321496
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-04 10:47:09 +0100

    delete console log from the code

--- src/views/correctAnswerView.js
+++ src/views/correctAnswerView.js
@@ -6,7 +6,6 @@ const answer = [];
 export const getSelected = () => {
   // we can use this function in the questions view to show the rocket when user select the correct answer after each question
   let answerEls = document.querySelectorAll('input[name="answer"]');
-  console.log('answerEls', answerEls);
   answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
       // we have to use checked here because we are getting all 4 inputs
