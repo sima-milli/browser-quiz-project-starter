4d1b6779b1895c2ba7d4e991a2e47cc300f05e8f
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-05 10:15:57 +0100

    update score in real time starting from the second question

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -6,15 +6,13 @@ import { quizData } from '../data.js';
  * @returns {Element}
  */
 
-// We can use the same idea here.
 export const createQuestionElement = (question) => {
   const element = document.createElement('div');
-
-  // I use String.raw just to get fancy colors for the HTML in VS Code.
-  element.innerHTML = String.raw`
+  if (quizData.currentQuestionIndex === 0) {
+    element.innerHTML = String.raw`
     <h1>${quizData.currentQuestionIndex + 1}/${
-    quizData.questions.length
-  } ${question}</h1>
+      quizData.questions.length
+    } ${question}</h1>
 
     <ul id="${ANSWERS_LIST_ID}">
     </ul>
@@ -23,5 +21,21 @@ export const createQuestionElement = (question) => {
       Next question
     </button>
   `;
+  } else {
+    element.innerHTML = String.raw`
+  <h1>${quizData.currentQuestionIndex + 1}/${
+      quizData.questions.length
+    } ${question}</h1>
+  <h2> Your score is: ${quizData.scoreCount}</h2>
+
+  <ul id="${ANSWERS_LIST_ID}">
+  </ul>
+
+  <button id="${NEXT_QUESTION_BUTTON_ID}">
+    Next question
+  </button>
+`;
+  }
+
   return element;
 };
