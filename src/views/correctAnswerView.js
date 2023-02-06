97fe215f26e9410a7c3a7843d4532e7be3b9967d
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-06 22:24:18 +0100

    deleted comments

--- src/views/correctAnswerView.js
+++ src/views/correctAnswerView.js
@@ -1,13 +1,9 @@
 import { quizData } from '../data.js';
-
-// this function will get the answer provided by user
 export const getSelected = () => {
-  // we can use this function in the questions view to show the rocket when user select the correct answer after each question
   let answer;
   let answerEls = document.querySelectorAll('input[name="answer"]');
   answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
-      // we have to use checked here because we are getting all 4 inputs
       answer = answerEl.id;
     }
   });
@@ -19,7 +15,6 @@ export const getScore = () => {
   if (answer === quizData.questions[quizData.currentQuestionIndex].correct) {
     quizData.scoreCount++;
   }
-  // you can use else to do the steps for wrong answer (rocket going down for example)
 };
 
-// if you need more steps for correct answer flow it should go here
+
